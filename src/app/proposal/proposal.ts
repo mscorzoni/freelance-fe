7129d4d21ab62ebb3ed6214fdfff622b6aee50b0
Marcos Scorzoni <mlessa@gmail.com>
2018-08-22 20:38:49 +0100

    add proposal constructor

---
+++ src/app/proposal/proposal.ts
@@ -0,0 +1,12 @@
+export class Proposal {
+  constructor(
+    public id?: number,
+    public customer?: string,
+    public portfolio_url: string = 'http://',
+    public tools?: string,
+    public stimated_hours?: number,
+    public hourly_rate?: number,
+    public weeks_to_complete?: number,
+    public client_email?: string,
+    ) {}
+}
