5e84c1c3606676c9687ddd2b90a3d44805fed564
Marcos Scorzoni <mlessa@gmail.com>
2018-08-22 13:07:11 +0100

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomepageComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomepageComponent]
 })
 export class AppModule { }
--- src/app/homepage.component.ts
+++ src/app/homepage.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './homepage.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'freelance-fe';
+export class HomepageComponent {
 }
+
