9984ca077a024283b07eea6fa81fc3436f1b70e7
Marcos Scorzoni <mlessa@gmail.com>
2018-08-22 18:58:25 +0100

    refactor homepage and app templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: '<h1>Navigation</h1>',
+  templateUrl: 'app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
