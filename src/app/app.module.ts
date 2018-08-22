3625f92101b5d9efa34df79f7512ee9eeb2bed62
Marcos Scorzoni <mlessa@gmail.com>
2018-08-22 19:11:35 +0100

    create documents component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { DocumentsComponent } from './documents/documents.component';
+
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    DocumentsComponent,
   ],
   imports: [
     BrowserModule,
