8b79ebdecac048e6d905ff73e427bf24a389806d
Marcos Scorzoni <mlessa@gmail.com>
2018-08-22 19:45:31 +0100

    add documents mocks

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Document } from './document'
 
 @Component({
   selector: 'documents',
@@ -6,7 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent {
-
-  constructor() { }
-
+  documents: Document[] = [
+    {
+      title: 'My First Doc',
+      description: 'First Description',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com'
+    },
+    {
+      title: 'My Second Doc',
+      description: 'First Description',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com'
+    },
+    {
+      title: 'My Third Doc',
+      description: 'First Description',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com'
+    }
+  ]
 }
