28a35c699782721cc1665adc1e066c6de1894aea
Marcos Scorzoni <mlessa@gmail.com>
2018-08-22 22:14:36 +0100

    add create proposal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component' ;
 import { ProposalComponent } from './proposal/proposal.component';
+import { ProposalNewComponent } from './proposal/proposal-new.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
     DocumentsComponent,
     ProposalComponent,
     ProposalListComponent,
+    ProposalNewComponent,
   ],
   imports: [
     BrowserModule,
