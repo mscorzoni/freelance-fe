c8acbcd4c390508966683e732794c04ea03c4a0d
Marcos Scorzoni <mlessa@gmail.com>
2018-08-23 16:05:17 +0100

    add cards to documents component

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -14,21 +14,21 @@ export class DocumentsComponent {
       description: 'First Description',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://google.com'
+      image_url: 'https://blog.freelance.com/wp-content/uploads/2016/03/compétences-freelance.jpg'
     },
     {
       title: 'My Second Doc',
       description: 'First Description',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://google.com'
+      image_url: 'https://www.yolancer.com/wp-content/uploads/2016/11/Freelancing-1.jpg'
     },
     {
       title: 'My Third Doc',
       description: 'First Description',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://google.com'
+      image_url: 'http://youngtopublishing.com/wp-content/uploads/2017/02/Freelancer-Fair-couple.jpg'
     }
   ]
 }
